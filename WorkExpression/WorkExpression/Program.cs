b497386912dce5ed48565d20b090e9992a34f380
usausa <machi.pon@gmail.com>
2020-05-22 14:24:00 +0900

    Add work project

---
+++ WorkExpression/WorkExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
