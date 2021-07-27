7d59930664c9fd97602e894d04cb87fa33c78384
usausa <machi.pon@gmail.com>
2021-07-27 17:40:45 +0900

    Update IL sample

---
+++ WorkMapper/Example/WorkIL/Data.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkIL
+{
+    public class Source
+    {
+        public int Value { get; set; }
+    }
+
+    public class Destination
+    {
+        public int Value { get; set; }
+    }
+}
